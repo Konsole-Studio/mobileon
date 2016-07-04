83a7beb0283e488b708a56a440775238c014e0e1
pt-br <lucas.paixao@objectedge.com>
2016-07-04 18:27:24 -0300

    Improvements on url rewriter

--- app/scripts/core_functions.js
+++ app/scripts/core_functions.js
@@ -35,68 +35,18 @@ rewriteLink = function(link) {
       return link;
   }
   return link.replace(/((?:(?:(?:http(?:s?)):)?(?:\/\/)?(?:(?:[a-zA-Z0-9][a-zA-Z0-9\-]*)(?:\.[\.a-zA-Z0-9\-]*)|localhost))(?:\:[0-9]+)?)/gi, function(hostHH) {
-      var rewritten = rewriterCore(hostHH, env.secure === 'true', env.__catch_all__);
+      var rewritten = rewriterCore(hostHH);
       return rewritten;
   });
 }
 
-rewriterCore = function (hostHH, secure, catchAll) {
-  var parsedHost, sanitizedHost, prefix, missing ="", ctxRules, result;
-  prefix = hostHH.match(/^(?:https?:)?\/\//);
-  if (!prefix) {
-      missing = secure ? 'https://' : 'http://';
-  }
-  else if (prefix[0] === '//') {
-      missing = secure ? 'https:' : 'http:';
-  }
-  // parse the sanitized host into a url object
-  parsedHost = url.parse(sanitizedHost);
-  if (parsedHost.port !== null && !(parsedHost.port === '443' || parsedHost.port === '80')) {
-      // skip rewriting links using nonstandard ports
-      return hostHH;
-  }
-  // Prevent adding the trailing slash when formatting
-  parsedHost.pathname = "";
-  // load the rules or interpolate them if they are missing from the context
-  ctxRules = rules || [
-      { Proxy: 'http://' + env.host,
-          Upstream: 'http://' + env.source_host
-      },
-      { Proxy: 'https://' + env.host,
-          Upstream: 'https://' + env.source_host
-      },
-      { Proxy: 'http://' + env.host,
-          Upstream: 'http://' + env.source_host.replace(/^www\./, '')
-      },
-      { Proxy: 'https://' + env.host,
-          Upstream: 'https://' + env.source_host.replace(/^www\./, '')
-      }
-  ];
-  if (ctxRules[0]) {
-      for (var i = 0; i < ctxRules.length; i++) {
-          var rr = ctxRules[i];
-          var match = sanitizedHost.match(rr.Upstream);
-          if (rr.Direction !== 1 && match!==null && match.index===0) {
-              // We found a match
-              var proxy = url.parse(rr.Proxy);
-              // Prevent adding the trailing slash when formatting
-              proxy.pathname = "";
-              if (env.__catch_all_enabled__ === 'true') {
-                  if (!proxy.host.match(catchAll)) {
-                      // The host should have the catchall appended, but doesn't yet
-                      proxy.hostname = proxy.hostname + catchAll;
-                      //unsetting host will reformat the url for us
-                      proxy.host = undefined;
-                  }
-              }
-              result = url.format(proxy);
-              if (result.substring(0, missing.length) === missing) {
-                  result = result.substring(missing.length);
-              }
-              return result;
-          }
-      }
-  }
+rewriterCore = function (hostHH) {
+  // TODO: KEEP http or https protocol, fix links opening after /
+  hostHH = hostHH.replace(/www\./, '')
+                 .replace(/https:\/\//, '')
+                 .replace(/http:\/\//, '');
+  hostHH = '//mlocal.' + hostHH;
+  console.log(hostHH);
   return hostHH;
 }
 
