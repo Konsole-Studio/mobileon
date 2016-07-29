b3e5f5fbcdad97120352e96ce184049efc3c99b5
pt-br <lucas.paixao@objectedge.com>
2016-07-28 21:26:30 -0300

    PDP initial adjustments

--- app/scripts/pages/browse/pdp.js
+++ app/scripts/pages/browse/pdp.js
@@ -2,10 +2,39 @@ var pdp = function() {
 
   var init = function() {
     initProductPage();
+    initFixBreadcrumb();
+    initProductBlock();
   };
 
   initProductPage = function() {
     body.addClass('mc-pdp');
+  };
+
+  initFixBreadcrumb = function() {
+    var breadcrumbContainer = body.find('#breadcrumb');
+    for( var i=0; i <= 2; i++ ) {
+      var breadcrumbContents = breadcrumbContainer.contents();
+      breadcrumbContents.last().remove();
+    }
+  };
+
+  initProductBlock = function() {
+    var productTable = body.find('#detalhes > table');
+    var productTitle = productTable.find('tr:nth-of-type(2) h2');
+    var productEntrega = productTitle.find('img');
+    var productBlockContainer = productTable.find('tr:nth-of-type(3) table');
+    var productImage = productBlockContainer.find('tr:nth-of-type(1) > td:nth-child(1)');
+
+    productTable.addClass('mc-product-table');
+    productTitle.addClass('mc-product-title');
+    productBlockContainer.addClass('mc-product-block');
+    productImage.addClass('mc-product-image');
+
+    if( productEntrega ) {
+      var entregaBadge = '<div class="mc-product-entrega">Pronta Entrega</div>';
+      productEntrega.addClass('mc-hide');
+      productTitle.after(entregaBadge);
+    }
   };
 
   return {
